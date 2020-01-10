c8c09dcf011be57beb7019f0483ab002a3bbd344
dyazincahya <cahyadyazin@yahoo.com>
2020-01-10 13:51:01 +0700

    up

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+	define("CONF_HOSTNAME", 		"localhost");
+	define("CONF_USERNAME", 		"root");
+	define("CONF_PASSWORD", 		"");
+	define("CONF_DATABASE", 		"db_helpdesk_web");
+	define("CONF_PRODIR", 		    "simple-helpdesk-php");
